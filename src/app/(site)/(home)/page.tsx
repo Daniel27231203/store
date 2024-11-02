cece04a61fbb9a4474ddcfaabd94d8dd3c06f7da
Ainazik <ainazikorosbekova54@gmail.com>
2024-11-02 23:37:01 +0600

    t4_create_footer

--- src/app/(site)/(home)/page.tsx
+++ src/app/(site)/(home)/page.tsx
@@ -1,6 +1,9 @@
 import Image from "next/image";
 import styles from "./page.module.css";
+import Footer from "@/components/layout/footer/Footer";
 
 export default function Home() {
-  return <>home</>;
+  return <>
+  <Footer/>
+  </>;
 }
