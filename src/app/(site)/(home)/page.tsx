68ebe49698e40bf0dad092aef2acf7795f9a04f9
karachy00 <kabylbek470@gmail.com>
2024-11-02 14:58:18 +0600

    t3_create_header

--- src/app/(site)/(home)/page.tsx
+++ src/app/(site)/(home)/page.tsx
@@ -1,6 +1,8 @@
 import Image from "next/image";
 import styles from "./page.module.css";
+import Header from "@/components/layout/Header/Header";
 
 export default function Home() {
-  return <>home</>;
+  return <>
+    </>;
 }
