78b60993c4c73248538c84c0b110b5281277cf3e
karachy00 <kabylbek470@gmail.com>
2024-11-13 14:56:41 +0600

    task #52: create finish homePage

--- src/app/(site)/(home)/page.tsx
+++ src/app/(site)/(home)/page.tsx
@@ -1,9 +1,6 @@
 import HomePage from "@/components/pages/HomePage";
-import Features from "@/components/pages/HomeSections/Features";
-import NewArrival from "@/components/pages/HomeSections/NewArrival";
 export default function Home() {
   return <>
     <HomePage/>
-    <Features/>
   </>;
 }
